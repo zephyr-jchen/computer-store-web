234da592042e7b6544c1e5caf057cd1f7214957e
Stella-KK <rkang56@uwo.ca>
2024-11-18 22:37:40 -0500

    2nd commit: built the navigation bar and the shopping cart button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,37 @@
-import {Container, Navbar as NavbarBs} from "react-bootstrap"
+import {Container, Button, Nav, Navbar as NavbarBs} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
 
 export function Navbar(){
     return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
-        <Container>Nav</Container>
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+        <Container>
+            <Nav className="me-auto">
+                <Nav.Link to="/" as={NavLink}>
+                Home
+                </Nav.Link>
+                <Nav.Link to="/customer" as={NavLink}>
+                Customer
+                </Nav.Link>
+                <Nav.Link to="/product" as={NavLink}>
+                Product
+                </Nav.Link>
+            </Nav>
+            <Button style={{width:"3rem", height:"3rem", backgroundColor:"lightgray", position:"relative"}}
+            className="rounded-circle">🛒
+                <div className="rounded-circle bg-danger d-flex justify-content-center
+                align-items-center"
+                style={{
+                    color:"white",
+                    width: "1.5rem",
+                    height: "1.5rem",
+                    position:"absolute",
+                    bottom:0,
+                    right:0,
+                    transform:"translate(20%,20%)",
+                }}>3</div>
+            </Button>
+        </Container>
     </NavbarBs>
     )
 }
