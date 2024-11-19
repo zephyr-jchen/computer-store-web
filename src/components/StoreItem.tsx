1f515146714f9e0384eb65d2c686ab0740c0bba5
Stella-KK <rkang56@uwo.ca>
2024-11-19 12:21:09 -0500

    3rd commit: built the user interface of the online computer store

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,43 @@
+import {Card, Button} from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
+import "../App.css"
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgUrl: string
+}
+
+export function StoreItem({id, name, price, imgUrl}:StoreItemProps){
+    const quantity = 0
+    
+    return <Card className="h-100">
+        <Card.Img variant="top" src={imgUrl} height="300px" style={{objectFit:"cover"}} />
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                <span className="fs-2">{name}</span>
+                <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+            </Card.Title>
+            <div className="mt-auto">
+                {quantity === 0? (
+                    // if quantity is 0, display add to cart button
+                    <Button className="w-100">Add To Cart</Button>
+                ):(
+                    // if quantity is not 0, display item quantity, +/- and remove button
+                    <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem" }}>
+                        <div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem" }}>
+                            <Button className="btn-qty">-</Button>
+                            <span className="fs-3">{quantity}</span>
+                            <Button className="btn-qty">+</Button>
+                        </div>
+                        <Button variant="danger" size="sm">
+                            Remove from Cart
+                        </Button>
+                    </div>
+                )}
+            </div>
+        </Card.Body>
+
+    </Card>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
+import storeItems from "../data/items.json"
+import {Row, Col} from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+
 export function Home(){
-    return <h1>Home</h1>
+    return (
+    <>
+        <h1>Computer Store</h1>
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {storeItems.map(item =>(
+                <Col key={item.id}>
+                <StoreItem {...item}/>
+                </Col>))}  
+        </Row>
+    </>
+    )
 }
